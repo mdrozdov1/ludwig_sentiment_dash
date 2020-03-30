fa49a175d727afc66c4c1bd803223cf604467bb2
dostoyevsky1 <mdrozdov94@gmail.com>
2020-03-29 23:37:53 -0400

    Create config module to set environment variables for python Dash

---
+++ dash/config.py
@@ -0,0 +1,6 @@
+import os
+
+ENVIRONMENT = os.environ.get("ENVIRONMENT", "dev")
+DEBUG = ENVIRONMENT == "dev"
+HOST = '0.0.0.0' if ENVIRONMENT == "prod" else 'localhost'
+API_URL = os.environ.get("API_URL", "http://localhost:3000")
